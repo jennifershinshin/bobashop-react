33bb0e93e882d46fb587a7e9e2071e0601221024
jennifershinshin <jennifershinshin@gmail.com>
2020-09-25 15:45:03 -0700

    made css module for buttons, made togglebutton function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import bobaHeaderImg from "./boba-header-img.jpg";
+import buttonStyle from "./Buttons.module.css";
+import "./App.css";
 
 function App() {
+  const toggleButtonColor = () => {
+    console.log("hello");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Momo Bubble Cafe</h1>
+        <div className="header-buttons">
+          <button className="make-boba-button">Make boba!</button>
+          <button className="about-button">About</button>
+          <button className="contact-button">Contact</button>
+        </div>
+        <img src={bobaHeaderImg} className="App-logo" alt="logo" />
       </header>
+      <h3>Tea</h3>
+      <div className={buttonStyle["tea-toppings"]}>
+        <button onClick={toggleButtonColor}>Matcha</button>
+        <button>Rose</button>
+        <button>Ceylon</button>
+      </div>
+      <h3>Toppings</h3>
+      <div className={buttonStyle}>
+        <button>Honey boba</button>
+        <button>Pudding</button>
+        <button>Lychee</button>
+        <button>Strawberries</button>
+      </div>
+      <h3>Your boba</h3>
     </div>
   );
 }
